b6288f976e9e1ecaaf4ab10d13a520c19365a209
MyLife4What <raweerot.p@ku.th>
2021-09-18 21:06:26 +0700

    Fix some error in models.py.

--- polls/models.py
+++ polls/models.py
@@ -7,8 +7,8 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    end_date = models.DateTimeField('ending date for voting')
-
+    end_date = models.DateTimeField('ending date for voting', null=True)
+    
     def __str__(self):
         return self.question_text
 
@@ -27,7 +27,7 @@ class Question(models.Model):
             bool : True if question was published.
         """
         now = timezone.now()
-        return self.end_date <= now
+        return self.pub_date <= now
 
     def can_vote(self):
         """Returns True if voting is currently allowed for this question
